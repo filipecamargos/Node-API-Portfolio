63a8d85ca86c95929f641fcc39802f52da0c97c5
filipecamargos <filipe.atrabalho@gmail.com>
2021-07-13 23:21:39 -0600

    Port returning DB connected

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 //Import the config file
 const CONFIG = require("./config");
 
 const app = express();
 
+//Register a body parser
+app.use(express.json());
+
 app.use("/", (req, res, next) => {
     console.log("live");
     return res.send("<h1>Test</h1>")
 })
 
-app.listen(8000);
+//Set the connection
+mongoose
+  .connect(CONFIG.db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(8000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
