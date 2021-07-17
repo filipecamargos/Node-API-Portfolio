8e30be4639c8de39b7d7546f2f030bcbe53d9280
filipecamargos <filipe.atrabalho@gmail.com>
2021-07-16 22:57:11 -0600

    Model Created

---
+++ model/project.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//The project schema to be saved in the DB
+const projectSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    description: { type: String, required: true },
+    stack: {type: String, required: true},
+    link: {type: String, required: true}
+  },
+  { timestamps: true } //Construct a timeStamp automatically 
+);
+
+module.exports = mongoose.model('Project', projectSchema)
