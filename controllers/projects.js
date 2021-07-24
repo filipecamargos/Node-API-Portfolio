31f42125f1ab2eb7b22f0f13b22721fec73c3dee
filipecamargos <filipe.atrabalho@gmail.com>
2021-07-24 13:39:30 -0600

    Project sucessfully updated in the DB

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,12 +3,12 @@ const Project = require("../model/project");
 /**
  * Get All the Projects
  */
- exports.getAllProjects = async (req, res, next) => {
+exports.getAllProjects = async (req, res, next) => {
   try {
     const projects = await Project.find();
     res.status(200).json(projects);
   } catch (err) {
-    res.status(401).json({
+    res.status(404).json({
       Message: "No collections found!",
       err,
     });
@@ -19,7 +19,6 @@ const Project = require("../model/project");
  * Create a Project
  */
 exports.createOneProject = async (req, res, next) => {
-
   //Instantiate the schema with the values received
   const project = new Project({
     title: req.body.title,
@@ -46,12 +45,11 @@ exports.createOneProject = async (req, res, next) => {
  */
 exports.getProjectById = async (req, res, next) => {
   const projectId = req.params.projectId;
-
   try {
     let project = await Project.findById(projectId);
     res.status(200).json(project);
   } catch (err) {
-    res.status(401).json({
+    res.status(404).json({
       Message: "Project Not Found!",
       err,
     });
@@ -62,12 +60,25 @@ exports.getProjectById = async (req, res, next) => {
  * Edit A Project by ID
  */
 exports.editProjectById = async (req, res, next) => {
-  const id = req.params.projectId;
+  const projectId = req.params.projectId;
+  try {
+    const project = await Project.findById(projectId);
 
-  res.status(200).json({
-    test: "Edit A Project By ID",
-    id: id,
-  });
+    project.title = req.body.title;
+    project.imagePath = req.body.imagePath;
+    project.description = req.body.description;
+    project.stack = req.body.stack;
+    project.link = req.body.link;
+
+    const savedProject = await project.save();
+    res.status(201).json(savedProject);
+
+  } catch (err) {
+    res.status(502).json({
+      message: "Error updating data",
+      err,
+    });
+  }
 };
 
 /**
@@ -87,4 +98,3 @@ exports.deleteProjectById = async (req, res, next) => {
  */
 
 //General Error Handling Function
-
